a07dc4f8e3db2bb68d9351a02adf22bde67c41f7
phucnh0707 <nguyenhphuc0707@gmail.com>
2024-04-17 06:38:57 -0700

    Initial commit

---
+++ Firebase/Firebase/SignUpViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SignUpViewController.swift
+//  Firebase
+//
+//  Created by Nguyen Hong Phuc on 4/17/24.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+ 
+    @IBAction func dismiss_OnClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func signUpBtn(_ sender: Any) {
+     
+    }
+    
+}
---
+++ Firebase/Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase
+//
+//  Created by Nguyen Hong Phuc on 4/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
